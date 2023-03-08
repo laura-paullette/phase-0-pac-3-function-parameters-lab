9586000646a25ecba5c621afdbc4b543696d138b
laura-paullette <laura.paullette@student.moringaschool.com>
2023-03-08 22:38:26 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,21 @@
+function introduction(name = "Aki") {
+    return(`Hi, my name is ${name}.`);
+} 
+introduction(String);
+function introductionWithLanguage(name="Aki", language="Python"){
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+introductionWithLanguage(String);
+function introductionWithLanguageOptional(name="Gracie", language="JavaScript") {
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+} 
+
+
+
+    
+
+
+
+
+
+     
